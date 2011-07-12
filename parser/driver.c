7421328272eff428e25e62256d45c8557f9e250a
Raphael Paiva <raphaeldpaiva@gmail.com>
2011-07-12 13:48:39 -0300

    removendo mais prototipos desnecessarios

--- parser/driver.c
+++ parser/driver.c
@@ -3,6 +3,23 @@
 #include <string.h>
 #include "driver.h"
 
+#define SYNTAX_ERROR(message, args...) { printf("Sintax error in line %i: ", yylineno); printf(message, ## args); puts("\n"); exit(0); }
+#define NO_BINOP -1
+
+int yylex(void);
+
+extern int yylineno;
+extern FILE *outfile;
+extern char *filename;
+
+int token;
+
+CommListNode *commandl();
+Exp *simple();
+Block *block();
+Declr *declr(DeclrListNode *root, int from_block, int from_function);
+DeclrListNode *declrs(int from_block, int from_function);
+
 void match(int next) {
 	if(token != next) {
 		printf("missing token: %c was %c\n", next, token);
