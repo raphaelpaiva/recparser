23838e44aa40fbfa39159da0255ec7db35ab392e
Raphael Paiva <raphaeldpaiva@gmail.com>
2011-07-12 13:44:41 -0300

    retirando prototipos desnecessarios

--- cfg/ast_utils.h
+++ cfg/ast_utils.h
@@ -12,13 +12,9 @@ extern "C" {
 
 using namespace std;
 
-string retrieve_command_tag(int tag);
-string retrieve_expression_tag(int tag);
 string retrieve_operation_string(int op);
 void error(string message);
-ostream& operator<<(ostream& o, Command& ast_command);
 void error(string message, Command *ast_command);
-ostream& operator<<(ostream& o, Exp& ast_command);
 void error(string message, Exp *ast_expression);
 
 #endif
--- letscode.sh
+++ letscode.sh
@@ -1,1 +1,1 @@
-gedit mongac.cpp parser/driver.c parser/driver.h ast/ast.h cfg/cfg_gen.h cfg/cfg_gen.cpp cfg/cfg.h cfg/cfg.cpp cfg/ast_utils.h cfg/ast_utils.cpp &
+gedit mongac.cpp ast/ast.h cfg/cfg_gen.cpp cfg/cfg.h cfg/cfg.cpp cfg/ast_utils.h cfg/ast_utils.cpp &
